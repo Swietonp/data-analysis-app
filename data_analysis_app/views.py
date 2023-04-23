dce1f1edace843dad25b29b082fd6a4c26cdd719
swietonp <swietonp@icloud.com>
2023-04-23 17:44:16 +0200

    messages system + forms for file input

--- data_analysis_app/views.py
+++ data_analysis_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def main_page(request):
+
+    if request.method == "POST":
+        messages.success(request, "Success!")
+
     return render(request, 'data_analysis_app/index.html')
