b724069d492ca07ab0d8510c47da3b1de4612d1d
swietonp <swietonp@icloud.com>
2023-04-23 16:43:30 +0200

    main view and base view defined

--- data_analysis/settings.py
+++ data_analysis/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data_analysis_app.apps.DataAnalysisAppConfig'
 ]
 
 MIDDLEWARE = [
--- data_analysis_app/views.py
+++ data_analysis_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, 'data_analysis_app/index.html')
