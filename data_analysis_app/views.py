c6454524dfc14b69f54bc07c84b272419d387614
swietonp <swietonp@icloud.com>
2023-05-02 14:05:01 +0200

    additional check: if no data given -> error msg

--- data_analysis_app/views.py
+++ data_analysis_app/views.py
@@ -6,16 +6,19 @@ from .data_processing import load_data
 def main_page(request):
 
     if request.method == "POST":
-        file = request.FILES['file']
-        data = load_data(file)
-        if data is None:
-            messages.error(request, 'Incorrect data!')
-        else:
-            messages.success(request, 'Success!')
-            request.session['data'] = data.to_html(
-                index=False, classes='table table-striped table-bordered table-hover table-sm')\
-                .replace('<tr style="text-align: right;">', '<tr>')
-            return redirect('/results')
+        try:
+            file = request.FILES['file']
+            data = load_data(file)
+            if data is None:
+                messages.error(request, 'Incorrect data!')
+            else:
+                messages.success(request, 'Success!')
+                request.session['data'] = data.to_html(
+                    index=False, classes='table table-striped table-bordered table-hover table-sm')\
+                    .replace('<tr style="text-align: right;">', '<tr>')
+                return redirect('/results')
+        except:
+            messages.error(request, 'No data!')
 
     return render(request, 'data_analysis_app/index.html')
 
