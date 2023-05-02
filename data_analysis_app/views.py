4915d3d47c3fe5057accd22809d385fc0e065495
swietonp <swietonp@icloud.com>
2023-05-02 14:55:30 +0200

    summary table added

--- data_analysis_app/views.py
+++ data_analysis_app/views.py
@@ -16,6 +16,9 @@ def main_page(request):
                 request.session['data'] = data.to_html(
                     index=False, classes='table table-striped table-bordered table-hover table-sm')\
                     .replace('<tr style="text-align: right;">', '<tr>')
+                request.session['summary'] = data.describe().to_html(
+                    classes='table table-hover table-light')\
+                    .replace('<tr style="text-align: right;">', '<tr>')
                 return redirect('/results')
         except:
             messages.error(request, 'No data!')
@@ -26,7 +29,8 @@ def main_page(request):
 def results_page(request):
 
     context = {
-        'data': request.session.get('data')
+        'data': request.session.get('data'),
+        'summary': request.session.get('summary')
     }
 
     return render(request, 'data_analysis_app/results.html', context)
